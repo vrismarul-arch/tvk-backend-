f2b1d53bc0955a9058b8ffa7478f09265dfff065
vrismarul-arch <vrismarul@gmail.com>
2026-05-14 19:53:59 +0530

    event

--- config/supabase.js
+++ config/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 
 export const supabase = createClient(
-  "https://yhagdvibohwzllsvtfuv.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InloYWdkdmlib2h3emxsc3Z0ZnV2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2OTE3NzIyOCwiZXhwIjoyMDg0NzUzMjI4fQ.A5j0EgPwKJXm8qVErLRWf65aWgPo3BLttrY9GnQcyh0"
+  "https://bwglgjteqloufayiaadv.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3Z2xnanRlcWxvdWZheWlhYWR2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NTQ5Nzg5MSwiZXhwIjoyMDcxMDczODkxfQ.DHN_CAuWSo0zD3JxZwCA5m5-laqULtNEZn0iu-Ywvlw"
 );
